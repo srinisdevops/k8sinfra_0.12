c05d627baf0e26e64c6b2f40e5cad6df1bf17a2f
srimul <60817863+srimul@users.noreply.github.com>
2020-05-23 10:03:35 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,6 +7,9 @@ provider "aws" {
 output "k8s_public_ip" {
   value = "${aws_instance.cZServers.*.public_ip}"
 }
+output "k8s_instance_id" {
+  value = "${aws_instance.cZServers.*.id}"
+}
 
 resource "aws_instance" "cZServers" {
   ami = var.amiid
